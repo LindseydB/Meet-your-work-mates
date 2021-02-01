278b6ce7f8f66f68217e309484e0b918472f4185
Lindsey Myers <lindsey@switched.com.au>
2021-02-02 10:56:26 +1300

    Styled the meetup tiles

--- client/src/components/home/meetups-list.component.js
+++ client/src/components/home/meetups-list.component.js
@@ -6,9 +6,65 @@ export default class MeetupList extends Component{
       <div>
 
       <div class="meetups">
-        <div class="meetup"><p>Event</p></div>
-        <div class="meetup"><p>Event</p></div>
-        <div class="meetup"><p>Event</p></div>
+        <div class="meetup">
+          <div class="row">
+            <div class="col-12">
+                <div class="eventimage"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="eventtitle">
+                <p>Event Name</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="eventinfo">
+                <p>users</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="meetup">
+          <div class="row">
+            <div class="col-12">
+                <div class="eventimage"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="eventtitle">
+                <p>Event Name</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="eventinfo">
+                <p>users</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="meetup">
+          <div class="row">
+            <div class="col-12">
+                <div class="eventimage"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="eventtitle">
+                <p>Event Name</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="eventinfo">
+                <p>users</p>
+                </div>
+            </div>
+          </div>
+        </div>
 
 
       </div>
