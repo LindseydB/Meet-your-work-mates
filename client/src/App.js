4a10856423605b84b395a6097867ebdf269835d0
Lindsey Myers <lindsey@switched.com.au>
2021-02-02 07:53:59 +1300

    Removed header border from login/register page

--- client/src/App.js
+++ client/src/App.js
@@ -81,7 +81,7 @@ function App() {
 
 
       <main class="col-md-12">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div class="d-flex">
 
         </div>
 
