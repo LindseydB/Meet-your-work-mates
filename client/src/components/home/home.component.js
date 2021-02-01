ba50a67f04dd255e0d79128f82a5bba020c38d6b
Lindsey Myers <lindsey@switched.com.au>
2021-02-02 10:35:30 +1300

    Adding content to dashboard event tiles

--- client/src/components/home/home.component.js
+++ client/src/components/home/home.component.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import EventList from "./event-list.component.js"
+import MeetupList from "./meetups-list.component.js"
+
 import "./home.css";
 
 
@@ -55,10 +57,10 @@ export default class CreateTodo extends Component{
     return(
       <div>
         <div class="row">
-          <div class="col-12">
-            <h1 class="h2">Hi, </h1>
+          <div class="col-12"><br />
+            <h1 class="h2">Hi Username, </h1><br />
+            <p><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</strong></p><br /><br />
             <h1 class="h2">Upcoming Events</h1>
-
             <div class="sidescroll">
               <EventList />
             </div>
@@ -67,7 +69,10 @@ export default class CreateTodo extends Component{
 
         <div class="row">
           <div class="col-12">
-            <h1 class="h2">Meet-ups</h1>
+            <h1 class="h2">Meet-ups</h1><br />
+            <div class="sidescroll">
+              <MeetupList />
+            </div>
             </div>
         </div>
       </div>
