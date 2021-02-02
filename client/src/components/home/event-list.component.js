567d5329534fc8d303d5f6f18eef4784b5d159fa
Lindsey Myers <lindsey@switched.com.au>
2021-02-02 15:10:19 +1300

    Added scrolling event tiles to the dashboard

--- client/src/components/home/event-list.component.js
+++ client/src/components/home/event-list.component.js
@@ -4,45 +4,52 @@ export default class EventList extends Component{
   render() {
     return(
       <div>
-      <div class="events">
-        <div class="event">
-          <div class="row">
-            <div class="col-4">
-              <div class="profileimg">
-              </div>
-            </div>
 
-           <div class="col-8">
-              <p><strong>Name<br />
-              123 Somewhere rd<br />
-              12pm<br />
-              02/02/2021</strong></p>
-              </div>
+      <div class="container-fluid">
+          <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
+      			<div class="col-5">
+            <div class="event">
+              <div class="row">
+                <div class="col-4">
+                  <div class="profileimg">
+                  </div>
+                </div>
+               <div class="col-8">
+                  <p><strong>Name<br />
+                  123 Somewhere rd<br />
+                  12pm<br />
+                  02/02/2021</strong></p>
+                  </div>
+                </div>
             </div>
+      		</div>
+
+
+          <div class="col-5">
+          <div class="event">
+            <div class="row">
+              <div class="col-4">
+                <div class="profileimg">
+                </div>
+              </div>
+             <div class="col-8">
+                <p><strong>Name<br />
+                123 Somewhere rd<br />
+                12pm<br />
+                02/02/2021</strong></p>
+                </div>
+              </div>
+          </div>
         </div>
+
+
+        <div class="col-5">
         <div class="event">
           <div class="row">
             <div class="col-4">
               <div class="profileimg">
               </div>
             </div>
-
-           <div class="col-8">
-              <p><strong>Name<br />
-              123 Somewhere rd<br />
-              12pm<br />
-              02/02/2021</strong></p>
-              </div>
-            </div>
-        </div>
-
-        <div class="event">
-          <div class="row">
-            <div class="col-4">
-              <div class="profileimg">
-              </div>
-            </div>
-
            <div class="col-8">
               <p><strong>Name<br />
               123 Somewhere rd<br />
@@ -52,7 +59,64 @@ export default class EventList extends Component{
             </div>
         </div>
       </div>
+
+
+      <div class="col-5">
+      <div class="event">
+        <div class="row">
+          <div class="col-4">
+            <div class="profileimg">
+            </div>
+          </div>
+         <div class="col-8">
+            <p><strong>Name<br />
+            123 Somewhere rd<br />
+            12pm<br />
+            02/02/2021</strong></p>
+            </div>
+          </div>
+      </div>
     </div>
+
+    <div class="col-5">
+    <div class="event">
+      <div class="row">
+        <div class="col-4">
+          <div class="profileimg">
+          </div>
+        </div>
+       <div class="col-8">
+          <p><strong>Name<br />
+          123 Somewhere rd<br />
+          12pm<br />
+          02/02/2021</strong></p>
+          </div>
+        </div>
+    </div>
+  </div>
+
+
+
+      <div class="col-5">
+      <div class="event">
+        <div class="row">
+          <div class="col-4">
+            <div class="profileimg">
+            </div>
+          </div>
+         <div class="col-8">
+            <p><strong>Name<br />
+            123 Somewhere rd<br />
+            12pm<br />
+            02/02/2021</strong></p>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
     )
   }
 }
