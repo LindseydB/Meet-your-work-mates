73ac41237ba3e26ba087f3a89f528c51983728ad
Dylan <dylandavy01@gmail.com>
2021-02-03 17:42:59 +1300

    Testing Husky

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,11 @@ import "./bootstrap/css/bootstrap.min.css";
 import Shell from "./containers/Shell";
 
 function App(props) {
-    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
     return (
         <Router>
             <Shell/>
         </Router>
     )
-    
-    }
-
+}
 
 export default App;
