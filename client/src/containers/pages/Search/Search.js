5a888f39c71fee89d22a0d422ceb8a76bce201a4
Dylan <dylandavy01@gmail.com>
2021-02-04 16:24:00 +1300

    Added basic search UI and form
    Minor code cleanups attached

--- client/src/containers/pages/Search/Search.js
+++ client/src/containers/pages/Search/Search.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "../../../bootstrap/css/bootstrap.min.css";
+import AdvancedSearch from "../../../components/AdvancedSearch/AdvancedSearch.js"
 
 const Search = () => {
+    const [advancedOptions, setAdvancedOptions] = useState(false);
+
+    const advancedButton = () => setAdvancedOptions(!advancedOptions);
+
     const onSubmit = (e) => {
         e.preventDefault();
         
@@ -13,12 +19,15 @@ const Search = () => {
             <h1>Find your fellow teammates</h1><br />
             <div>
                 <form onSubmit={onSubmit}>
-                    <div className="form-group">
+                    <div className="row">
                         <input type="text"
                             name="keywords"
                             placeholder="Input your search terms"
-                            required />
+                            required
+                            className="col-9" />
+                        <button type="button" name="advanced button" className="col-3" onClick={advancedButton}>Advanced</button>
                     </div>
+                    {advancedOptions === false ? null : <AdvancedSearch />} {/* need to add a margin here, not sure how */}
                     <div className="form-group">
                         <input type="submit" value="Search" className="btn btn-primary" />
                     </div>
