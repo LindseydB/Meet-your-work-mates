f97bcd2c577f0f256dc880cd5f292bcdf0ead3d7
ddjmhatre <ddjmhatre@gmail.com>
2021-02-08 18:59:12 +1300

    Fixed Profile.js to remove error

--- client/src/containers/pages/Profile/Profile.js
+++ client/src/containers/pages/Profile/Profile.js
@@ -9,15 +9,7 @@ const Profile = () => {
   <h3>Name</h3>
 </div>
 
-<TextField
-id="multiline-flexible"
-label="About"
-multiline
-rows="2"
-value={this.state.about}
-onChange={this.handleChange('about')}
-/>
-</div>
+
 )
 
     
