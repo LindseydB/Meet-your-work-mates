bdca3268e644805dbb8ebf877825dac75b5a2b43
ddjmhatre <ddjmhatre@gmail.com>
2021-02-08 19:05:41 +1300

    Profile edited again

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./bootstrap/css/bootstrap.min.css";
 import Shell from "./containers/Shell";
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 function App(props) {
     return (
         <Router>
