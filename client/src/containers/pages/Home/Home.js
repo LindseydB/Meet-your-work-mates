6525fa2518d7bb438475bcef6de122b252fa06e4
waltllowe <waltllowe@users.noreply.github.com>
2021-02-10 10:59:33 +1300

    stores current user from login in local storage

--- client/src/containers/pages/Home/Home.js
+++ client/src/containers/pages/Home/Home.js
@@ -3,12 +3,14 @@ import EventList from "../../../components/EventList/EventList.js"
 import MeetupList from "../../../components/MeetupsList/MeetupsList.js"
 import "./home.css";
 
+const CurrentUser = localStorage.getItem('currentUser');
+
 const Home = () => {
   return (
     <div>
       <div className="row">
-        <div className="col-12"><br />
-          <h1 className="h2">Hi Username, </h1>
+              <div className="col-12"><br />
+                  <h1 className="h2">Hi {JSON.parse(CurrentUser).name}, </h1>
           <p><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</strong></p><br /><br />
           <h1 className="h2">Catch-ups</h1>
           <div className="sidescroll">
