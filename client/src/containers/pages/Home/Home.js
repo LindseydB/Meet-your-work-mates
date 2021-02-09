2d6e05466cf167a4fc6b01ca80b0b579ba15097a
waltllowe <waltllowe@users.noreply.github.com>
2021-02-10 11:56:22 +1300

    updated getting user name

--- client/src/containers/pages/Home/Home.js
+++ client/src/containers/pages/Home/Home.js
@@ -3,14 +3,13 @@ import EventList from "../../../components/EventList/EventList.js"
 import MeetupList from "../../../components/MeetupsList/MeetupsList.js"
 import "./home.css";
 
-const CurrentUser = localStorage.getItem('currentUser');
 
 const Home = () => {
   return (
     <div>
       <div className="row">
               <div className="col-12"><br />
-                  <h1 className="h2">Hi {JSON.parse(CurrentUser).name}, </h1>
+                  <h1 className="h2">Hi {JSON.parse(localStorage.getItem('currentUser')).name}, </h1>
           <p><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</strong></p><br /><br />
           <h1 className="h2">Catch-ups</h1>
           <div className="sidescroll">
