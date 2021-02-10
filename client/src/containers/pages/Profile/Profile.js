d640b63ad81892647584119db6f9ac5d6763b131
ddjmhatre <ddjmhatre@gmail.com>
2021-02-11 10:48:02 +1300

    Update Profile.js

--- client/src/containers/pages/Profile/Profile.js
+++ client/src/containers/pages/Profile/Profile.js
@@ -15,39 +15,6 @@ export default class Profile extends Component {
    
     render() {
         return (
-<<<<<<< Updated upstream
-            <div>
-                <button
-                // onClick={this.upload.bind(this)}
-                >
-                    Edit Profile
-                </button>
-
-
-                <div style={{ marginTop: 10 }}>
-                    <h3>Annabel Username</h3>
-
-                    <div>
-                        <h5>Picture Uploader</h5>
-
-                        <input
-                            type="file"
-                        // onChange={this.handlePictureSelected.bind(this)}
-                        />
-                        <br />
-                        <div>
-                            {/* {this.renderPreview()} */}
-                        </div>
-                        <hr />
-                        <button
-                        // onClick={this.upload.bind(this)}
-                        >
-                            Upload
-                        </button>
-                    </div>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-=======
         <div>
           <button
         
@@ -79,63 +46,11 @@ export default class Profile extends Component {
       </div>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
->>>>>>> Stashed changes
                             <input type="text"
                                 className="form-control"
                                 value="orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis dolor eget libero porta, et tristique mauris maximus. Suspendisse potenti. Etiam efficitur iaculis libero sit amet laoreet. Sed ornare, eros sit amet laoreet interdum, dolor leo posuere ligula, sit amet rutrum lectus nisi quis metus. Nunc sodales ipsum consectetur sapien auctor rutrum. Quisque vulputate quis turpis sit amet posuere. Aenean fringilla turpis arcu, quis semper libero facilisis eu. Vivamus gravida ante mi, non pharetra ex ultrices nec. Pellentesque vitae neque felis.
 
                             Nam molestie quam sit amet dui condimentum, fringilla rutrum justo cursus. Sed quis ornare tellus. Mauris urna eros, pretium at enim"
-<<<<<<< Updated upstream
-                            // value={this.state.todo_description}
-                            // onChange={this.onChangeTodoDescription}
-                            />
-                        </div >
-                        <div className="form-group">
-                            <label>Role: </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                // value={this.state.todo_responsible}                          
-                                onChange={this.onChangeTodoResponsible}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Department: </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                // value={this.state.todo_responsible}                          
-                                onChange={this.onChangeTodoResponsible}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Location: </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                // value={this.state.todo_responsible}                          
-                                onChange={this.onChangeTodoResponsible}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Interests: </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                // value={this.state.todo_responsible}                          
-                                onChange={this.onChangeTodoResponsible}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Skills: </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                // value={this.state.todo_responsible}                          
-                                onChange={this.onChangeTodoResponsible}
-                            />
-                        </div>
-=======
                        
                         />
                     </div >
@@ -185,7 +100,6 @@ export default class Profile extends Component {
                         />
                     </div>           
                                          
->>>>>>> Stashed changes
 
 
                         <div className="form-group">
