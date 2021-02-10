6226e60b67cf9231d1013fabc1505beabe83bf75
Lindsey Myers <lindsey@switched.com.au>
2021-02-10 14:58:51 +1300

    Uncommenting line in login

---
+++ client/src/components/PastMeetupsList/PastMeetupsList.js
@@ -0,0 +1,214 @@
+import React,{Component} from 'react';
+
+export default class PastMeetupList extends Component{
+  render() {
+    return(
+      <div>
+
+
+
+            <div className="container-fluid">
+                <div className="scrolling-wrapper2 row flex-row flex-nowrap mt-4 pb-4 pt-2">
+
+                   <div className="meetups">
+                    <div className="meetup">
+                      <div className="row">
+                        <div className="col-12">
+
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="eventtitle">
+                            <p span class="sml">6pm - 2am</p>
+                            <p span class="eventName">Event Name</p>
+                            <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="eventinfo">
+                                <div class="smlCircle"></div>
+                                <div class="smlCircle"></div>
+                                <div class="smlCircle"></div>
+                                <div class="counter">7+</div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="meetups">
+                   <div className="meetup">
+                     <div className="row">
+                       <div className="col-12">
+
+                       </div>
+                     </div>
+                     <div className="row">
+                       <div className="col-12">
+                         <div className="eventtitle">
+                           <p span class="sml">6pm - 2am</p>
+                           <p span class="eventName">Event Name</p>
+                           <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                           </div>
+                       </div>
+                       <div className="col-12">
+                           <div className="eventinfo">
+                               <div class="smlCircle"></div>
+                               <div class="smlCircle"></div>
+                               <div class="smlCircle"></div>
+                               <div class="counter">7+</div>
+                           </div>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+
+                 <div className="meetups">
+                  <div className="meetup">
+                    <div className="row">
+                      <div className="col-12">
+
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-12">
+                        <div className="eventtitle">
+                          <p span class="sml">6pm - 2am</p>
+                          <p span class="eventName">Event Name</p>
+                          <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                          </div>
+                      </div>
+                      <div className="col-12">
+                          <div className="eventinfo">
+                              <div class="smlCircle"></div>
+                              <div class="smlCircle"></div>
+                              <div class="smlCircle"></div>
+                              <div class="counter">7+</div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="meetups">
+                 <div className="meetup">
+                   <div className="row">
+                     <div className="col-12">
+
+                     </div>
+                   </div>
+                   <div className="row">
+                     <div className="col-12">
+                       <div className="eventtitle">
+                         <p span class="sml">6pm - 2am</p>
+                         <p span class="eventName">Event Name</p>
+                         <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                         </div>
+                     </div>
+                     <div className="col-12">
+                         <div className="eventinfo">
+                             <div class="smlCircle"></div>
+                             <div class="smlCircle"></div>
+                             <div class="smlCircle"></div>
+                             <div class="counter">7+</div>
+                         </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+
+               <div className="meetups">
+                <div className="meetup">
+                  <div className="row">
+                    <div className="col-12">
+
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="eventtitle">
+                        <p span class="sml">6pm - 2am</p>
+                        <p span class="eventName">Event Name</p>
+                        <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <div className="eventinfo">
+                            <div class="smlCircle"></div>
+                            <div class="smlCircle"></div>
+                            <div class="smlCircle"></div>
+                            <div class="counter">7+</div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="meetups">
+               <div className="meetup">
+                 <div className="row">
+                   <div className="col-12">
+
+                   </div>
+                 </div>
+                 <div className="row">
+                   <div className="col-12">
+                     <div className="eventtitle">
+                       <p span class="sml">6pm - 2am</p>
+                       <p span class="eventName">Event Name</p>
+                       <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                       </div>
+                   </div>
+                   <div className="col-12">
+                       <div className="eventinfo">
+                           <div class="smlCircle"></div>
+                           <div class="smlCircle"></div>
+                           <div class="smlCircle"></div>
+                           <div class="counter">7+</div>
+                       </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+             <div className="meetups">
+              <div className="meetup">
+                <div className="row">
+                  <div className="col-12">
+
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                    <div className="eventtitle">
+                      <p span class="sml">6pm - 2am</p>
+                      <p span class="eventName">Event Name</p>
+                      <p span class="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                      </div>
+                  </div>
+                  <div className="col-12">
+                      <div className="eventinfo">
+                          <div class="smlCircle"></div>
+                          <div class="smlCircle"></div>
+                          <div class="smlCircle"></div>
+                          <div class="counter">7+</div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+        </div>
+      </div>
+     </div>
+
+    )
+  }
+}
